7a1c5cc0a965b4859865a73275a57bae23736939
Alex E. Rodriguez <arodriguez23434@knights.ucf.edu>
2016-07-07 00:58:54 -0400

    Time Consideration & Multiple Iterations
    Updates:
    - Added map background and moved nodes accordingly
    - Removed nodeNeighbors from config and added edgeAttr
    > Key is Node1,Node2; X = Obstructed, Y = List of Times
    - Modified EXCEL format
    > Worksheet 1
    o Modified Columns B & C to be specific to grid/display
    o Columns D and E now reserved for real world coordinates
    > Worksheet 2
    o Row 1 has entries Node1,Node2 (key of PathMatrix)
    o Row 2 indicates whether the edge is obstructed (boolean)
    o Rows 3+ is time data entries in seconds
    - Modified file_excel_interpret to obtain nodeNeighbors from Sheet 2 Row
    1 and create edgeAttr from Sheet 2
    - Added numRuns to configuration (# of iterations for a particular
    program run)
    - edgeAttr and numRuns added to check_user_input
    - Overhauled calc_edge_weights for time consideration and multiple
    iterations
    > Mean and standard deviations of edge times in edgeAttr stored as
    variables
    > Now generates a list of sample times (index count = numRun) from mean
    and std. dev. using numpy.random.normal (Gaussian Noise)
    > Factors obstructions into edges (weight = 999999 if True)
    > Now has attributes 'weight_list' and 'time_list' formed from generated
    list of times
    - Modified get_shortest_path_info for multiple iterations
    > Now obtains total 'weight' from the edges on the path instead of dist
    and return that as info[0]
    > Also obtains total 'time' from edges on path and return that as
    info[2] and total 'distance' as info[3]
    - Modified matrix_create for multiple iterations
    > Now iterates over numRuns and sets 'weight' = 'weight_list'[iter] for
    each path consideration
    > Stores both best path from all iterations as well as all considered
    paths into pathMatrix[key][0] and pathMatrix[key][1] respectively
    - Graph now displays best path from multiple iterations, travel time on
    that path, distance on that path, and number of iterations performed
    
    Todo:
    - Modify network_add_node and network_remove_node
    > Account for edgeAttr
    > Account for numRuns
    > Modify example calls accordingly
    - inputs.txt
    > Make lists of origins and destinations and have user pick/comment out
    > Number of iterations in file
    > Whether to output/log data T/F
    - Handle obstructed edges more cleanly (currently just sets weight to
    999999)
    > If edge is obstructed, set color to red
    - Update syllabus to reflect changes
    - Update syllabus to include report desired by EXCEL

--- src/func_network_operations.py
+++ src/func_network_operations.py
@@ -3,6 +3,8 @@
 import math
 import networkx as nx
 import openpyxl as xl
+import statistics as stat
+from numpy import random
 
 #Function to get various pieces of information of shortest path from startnode to endnode and output them
 #Currently obtains total path distance and all edges along path
@@ -14,28 +16,36 @@ def get_shortest_path_info(graph,startnode,endnode,netNodeAttr):
         print("ERROR: Node %s is unreachable from node %s. Is the path properly configured?" % (endnode,startnode))
         return False
     else:
-        #Sum distances between each node in list and return that sum
-        dist = 0; prevnode = startnode;
-        for currnode in best_route:
-            dist += math.sqrt(math.pow(netNodeAttr[currnode][0]-netNodeAttr[prevnode][0],2) + math.pow(netNodeAttr[currnode][1]-netNodeAttr[prevnode][1],2))
-            prevnode = currnode
-        #Get edges along path
-        edgelist = []; prevnode = startnode
+        #Get edges and their weights/times along path
+        edgelist = []; prevnode = startnode; 
+        weight_total = 0; time_total = 0; dist_total = 0
         for i in best_route:
             if (i!=startnode):
                 if (i,prevnode) in graph.edges():
                     edgelist.append((i,prevnode)) 
+                    weight_total+=graph[i][prevnode]['weight']
+                    time_total+=graph[i][prevnode]['time']
+                    dist_total+=graph[i][prevnode]['distance']
                 elif (prevnode,i) in graph.edges():
                     edgelist.append((prevnode,i))
+                    weight_total+=graph[prevnode][i]['weight']
+                    time_total+=graph[prevnode][i]['time']
+                    dist_total+=graph[prevnode][i]['distance']
                 else:
                     print("Could not find edge "+(prevnode,i))
             prevnode = i
         #Store and return all the information
-        info = [dist,edgelist]
+        info = [weight_total,edgelist,time_total,dist_total]
         return info;
 
+#Iterate over generated times/weights based on Gaussian distribution
+def iter_edges(graph,runIter):
+    for i,j in graph.edges():
+        graph[i][j]['weight']=graph[i][j]['weight_list'][runIter]
+        graph[i][j]['time']=graph[i][j]['time_list'][runIter]
+
 #Function to initialize the path matrix
-def matrix_create(graph,netNodeAttr):
+def matrix_create(graph,netNodeAttr,netNumRuns):
     print('Creating path matrix...');
     #Initialize dictionary representing path matrix
     pathMatrix = dict();
@@ -43,9 +53,24 @@ def matrix_create(graph,netNodeAttr):
     for i in netNodeAttr.keys():
         for j in netNodeAttr.keys():
             try:
-                #Calculate the distance between every path
-                pathinfo = get_shortest_path_info(graph,i,j,netNodeAttr)
-                if pathinfo!=False: pathMatrix[(i,j)] = pathinfo
+                #Create a list for each path over netNumRuns iterations and final "best of best" path 
+                best_path = list(); all_paths = list()
+                for runIter in range(0,netNumRuns):
+                    iter_edges(graph,runIter);
+                    #Calculate the distance between every path
+                    pathinfo = get_shortest_path_info(graph,i,j,netNodeAttr)
+                    #Only record info if path is valid                    
+                    if pathinfo!=False: 
+                        #If this is the first entry in best_path, set it to current path
+                        if not best_path: best_path = pathinfo
+                        #If the path we found is better than our best_path, set best_path to it
+                        elif pathinfo[0]<best_path[0]: best_path = pathinfo
+                        #Append the path to all potential paths
+                        all_paths.append(pathinfo)
+                    #If an error occurs, raise an exception
+                    else: raise nx.NetworkXNoPath()
+                #If the path lists are valid, set the pathMatrix entry to them
+                if best_path and all_paths: pathMatrix[(i,j)] = [best_path,all_paths]
                 else: raise nx.NetworkXNoPath()
             except:
                 print("ERROR: Failed to create pathing matrix. Please check your configuration and try again.");
@@ -53,11 +78,12 @@ def matrix_create(graph,netNodeAttr):
     return pathMatrix;
 
 #Function to remove a node from all network-related items
-def network_remove_node(node,graph,netNodeAttr,netNodePos,pathMatrix):
+def network_remove_node(node,graph,netNodeAttr,netEdgeAttr,netNodePos,pathMatrix):
     #Delete the node from the attribute and position dictionaries
     del netNodeAttr[node]; del netNodePos[node]
     #Delete the node from the NetworkX graph
     graph.remove_node(node)
+    #TODO: Remove references to node from edgeAttr
     #Remove all references to a node from path matrix
     try:
         newMatrix = {key:pathMatrix[key] for key in pathMatrix if node not in key[0] and node not in key[1]};
@@ -69,16 +95,18 @@ def network_remove_node(node,graph,netNodeAttr,netNodePos,pathMatrix):
         for i in newMatrix[key][1]:
             if i[0]==node or i[1]==node:
                 #Calculate the distance between every path
+                #TODO: Modify to new format based on netNumRuns
                 pathinfo = get_shortest_path_info(graph,key[0],key[1],netNodeAttr)               
                 #TODO: Multiple path attributes
                 newMatrix[key] = pathinfo;
     return newMatrix;
 
 #Function to add a node with attribute list addNodeAttr and list of connecting neighbors to all network-related items
-def network_add_node(node,addNodeAttr,neighbors,graph,netNodeAttr,netNodePos,pathMatrix):
+def network_add_node(node,addNodeAttr,neighbors,graph,netNodeAttr,netEdgeAttr,netNodePos,pathMatrix,netNumRuns):
     try: type(addNodeAttr[1])
     except: print("ERROR: Invalid node attribute input! (Must be list of at least 2 values)")
     else:    
+        #TODO: Add neighbors to edgeAttr
         #Add the node to the graph
         graph.add_node(node)
         graph.node[node]['pos'] = [addNodeAttr[0],addNodeAttr[1]]
@@ -92,16 +120,15 @@ def network_add_node(node,addNodeAttr,neighbors,graph,netNodeAttr,netNodePos,pat
         try:
             for connect in neighbors:           
                 try: 
-                    dist = math.sqrt(math.pow(netNodePos[connect][0]-netNodePos[node][0],2) + math.pow(netNodePos[connect][1]-netNodePos[node][1],2))
                     graph.add_edge(node,connect)
-                    graph[node][connect]['weight'] = dist
                 except KeyError: print("ERROR: Failed to connect node to neighbors! (Attempted to connect to nonexistent neighbor)")            
-                except TypeError: print("ERROR: Failed to connect node to neighbors! (Node attributes were not a list of at least 2 elements)")            
-        except TypeError: print("ERROR: Failed to connect node to neighbors! (Input type was not a list)")
+            calc_edge_weights(graph,netNodeAttr,netEdgeAttr,netNumRuns)
+        except TypeError: print("ERROR: Failed to connect node to neighbors! (Node attributes were not a list of at least 2 elements)")            
         #Retrieve all possible paths and add to path matrix
         try:
             for key in netNodeAttr.keys():
-                pathinfo = get_shortest_path_info(graph,node,key,netNodeAttr)
+                #TODO: Modify to new format based on netNumRuns
+                pathinfo = get_shortest_path_info(graph,node,key,netNodeAttr,netNumRuns)
                 pathMatrix[(node,key)] = pathinfo;
                 pathMatrix[(key,node)] = pathinfo;
         except TypeError:
@@ -109,13 +136,37 @@ def network_add_node(node,addNodeAttr,neighbors,graph,netNodeAttr,netNodePos,pat
             return False
     return pathMatrix;
 
-#TODO: Create edgeAttr dict for further weight calculations
-#TODO: Consider other nodeAttr attributes
-def calc_edge_weights(graph,netNodeAttr):
+def calc_edge_weights(graph,netNodeAttr,netEdgeAttr,netNumRuns):
     for node1,node2 in graph.edges():
-        dist = math.sqrt(math.pow(netNodeAttr[node2][0]-netNodeAttr[node1][0],2) + math.pow(netNodeAttr[node2][1]-netNodeAttr[node1][1],2));
-        result = dist
-        graph[node1][node2]['weight'] = result;
+        #Try to get info about the edge from edgeAttr
+        #Before throwing an exception, check if networkX ordered the key backwards
+        try: edgeInfo=netEdgeAttr[str(node1+','+node2)]
+        except KeyError: edgeInfo=netEdgeAttr[str(node2+','+node1)]
+        #Get the average and standard deviation of recorded times
+        timeAvg = stat.mean(edgeInfo[1]); timeStd = stat.stdev(edgeInfo[1]);
+        #Create a list for time generated based on Gaussian distribution
+        #Create a list for all the weights calculated based on the above times
+        timeGen = list(); weight_list = list()
+        #Calculate the distance between the nodes on the edge
+        dist = math.sqrt(math.pow(netNodeAttr[node2][2]-netNodeAttr[node1][2],2) + math.pow(netNodeAttr[node2][3]-netNodeAttr[node1][3],2));
+        for i in range(0,netNumRuns): 
+            #Generate a time based on Gaussian distribution of input times
+            timeGen.append(random.normal(timeAvg,timeStd,None))
+            #Add this time to the list of weights
+            weight_list.append(timeGen[i])
+            #Include distance as part of the weight operation
+            weight_list[i] += dist
+            #If the road is obstructed, set weight to an invalid value
+            #TODO: Currently it just sets the weight to a high value;
+            #      Find suitable NetworkX substitute if possible
+            if edgeInfo[0]==True: weight_list[i] = 999999;
+        #Record time list, weight list, and initial values as NetworkX edge attributes
+        graph[node1][node2]['time_list'] = timeGen;
+        graph[node1][node2]['time'] = timeGen[0];        
+        graph[node1][node2]['weight_list'] = weight_list;
+        graph[node1][node2]['weight'] = weight_list[0];
+        #TODO: Refine distance to be based on more accurate formula
+        graph[node1][node2]['distance'] = 86.121212*dist;
     return;
 
 def file_check(netFileDir):
@@ -133,9 +184,9 @@ def file_check(netFileDir):
         print("ERROR: File type was not supported (%s instead of .xlsx, .xlsm, .xltx, or .xltm)" % netFileDir[-4:])
         return False;
 
-def file_excel_interpret(netFileDir,wb,netNodeAttr,netNodeNeighbors,netPathDesired):
+def file_excel_interpret(netFileDir,wb,netNodeAttr,netEdgeAttr,netNodeNeighbors,netPathDesired):
     #Since we are no longer using the in-line config, clear inputs
-    netNodeAttr.clear(); netNodeNeighbors[:] = []
+    netNodeAttr.clear(); netEdgeAttr.clear(); netNodeNeighbors[:] = []
     #Load the 1st worksheet in the excel workbook  (nodeAttr)   
     ws = wb.worksheets[0]
     #Recreate the nodeAttr dictionary row-by-row
@@ -152,16 +203,29 @@ def file_excel_interpret(netFileDir,wb,netNodeAttr,netNodeNeighbors,netPathDesir
             #If the cell is in the label column, do not interpret
             if  cell.column == 'A': continue
             #Add each cell value to the temporary list of row values
-            row_values.append(cell.value*100)
+            row_values.append(cell.value)
         #Use first column value as key and add list of row values as attributes
         netNodeAttr[row[0].value] = row_values
     #Load the 2nd worksheet in the excel workbook (nodeNeighbors)
     ws = wb.worksheets[1]
     #Recreate nodeNeighbors list row-by-row    
-    for row in ws.rows: netNodeNeighbors.append([row[0].value,row[1].value])
+    for col in ws.columns: 
+        #TODO: Put under try method
+        #Create a temporary list of values for a column
+        col_values = list(); time_values = list()
+        #If the column is the label column, do not interpret
+        if col[0].column == 'A': continue
+        for cell in col:
+            if cell.row == 1:
+                strind = cell.value.index(',')
+                netNodeNeighbors.append([cell.value[0:strind],cell.value[strind+1:]])
+            elif cell.row == 2: col_values.append(cell.value)
+            else: time_values.append(cell.value)
+        col_values.append(time_values)
+        netEdgeAttr[col[0].value] = col_values                
     return netPathDesired;
     
-def check_user_input(netNodeAttr,netNodeNeighbors,netPathDesired):
+def check_user_input(netNodeAttr,netEdgeAttr,netNodeNeighbors,netPathDesired,netNumRuns):
     #Are the nodes properly setup?
     for i in netNodeAttr.values():
         #Is the node a list with at least two attributes?        
@@ -181,6 +245,19 @@ def check_user_input(netNodeAttr,netNodeNeighbors,netPathDesired):
                 return False;
         #TODO: Make temporary list of distances between positions and warn user
         #      if distances between a single pair are unreasonably large or zero
+    for i in netEdgeAttr:
+        #TODO: Check if key is two nodes seperated by comma (,)
+        try: i.index(',')
+        except:
+            print("ERROR: An edge was not properly setup! (Key should be in form of node1,node2; recieved {0})" % i)
+        #Is the edge a list with at least two attributes?        
+        try: [netEdgeAttr[i][0],netEdgeAttr[i][0]];
+        except TypeError:
+            print("ERROR: An edge was not properly setup! (Edge attributes were not entered as a list; entered as {0})".format(netEdgeAttr[i]))
+            return False;
+        except IndexError:
+            print("ERROR: An edge was not properly setup! (Not enough/invaild attributes; only found {0})".format(netEdgeAttr[i]))
+            return False;
     #Are the edges properly setup?
     for i in netNodeNeighbors:
         try:
@@ -223,4 +300,9 @@ def check_user_input(netNodeAttr,netNodeNeighbors,netPathDesired):
     elif node_occur_b<=0:
         print("ERROR: End node %s on desired route does not exist!" % netPathDesired[1])
         return False;
+    #Check if the number of iterations is reasonable
+    if netNumRuns < 1:
+        print("ERROR: Number of times to run program is less than 1 (input %d)!" % netNumRuns)
+        return False;
+    elif netNumRuns > 1000: print("WARNING: Large number of times to run program (input %d)!" % netNumRuns)
     return True;
--- src/main_matrix_model.py
+++ src/main_matrix_model.py
@@ -4,7 +4,9 @@ from pylab import rcParams
 import math
 import networkx as nx
 import matplotlib.pyplot as plt
+from scipy.misc import imread
 import func_network_operations as nop
+
 
 """
 Objective:
@@ -22,8 +24,11 @@ fileDir = 'files/helloworld.xlsx'
 #nodeNeighbors: Must be list of 2D lists that are made of two node names in nodeAttr
 #pathDesired: Must be a 2D tuple made of two node names in nodeAttr
 nodeAttr = {'Station': [10,8], 'Jay-Bergman Field': [8,8], 'Softball Field': [10,6], 'CFE Arena': [8,6], 'Lake Claire': [6,6], 'Child Center': [8,4], 'Milican Hall': [6,4]}
-nodeNeighbors = [['Station','Jay-Bergman Field'],['Station','Softball Field'],['Jay-Bergman Field','CFE Arena'],['Softball Field','CFE Arena'],['CFE Arena','Lake Claire'],['CFE Arena', 'Child Center'],['Child Center','Milican Hall'],['Lake Claire','Milican Hall']]
 pathDesired = ('Station','Milican Hall')
+edgeAttr = {'Station,Jay-Bergman Field': [False,[1]]}
+numRuns = 100
+#edgeAttr: Key is Node1,Node2; X = Obstructed, Y = List of Times
+#TODO: Get neighbors from keys
 #TODO: obstructedEdges = [['A','B'],...]
 #--End of Configuration--
 
@@ -48,19 +53,19 @@ def main():
         outGraph.node[i]['pos'] = p;
     try:
         #NetworkX uses a universal 'weight' attribute for edges for its algorithms
-        nop.calc_edge_weights(outGraph,nodeAttr)
+        nop.calc_edge_weights(outGraph,nodeAttr,edgeAttr,numRuns)
     except KeyError:
         print("ERROR: Found node in nodeNeighbors that does not exist in nodeAttr!")
         return
     #Create a path matrix for the network
-    pathMatrix = nop.matrix_create(outGraph,nodeAttr);
+    pathMatrix = nop.matrix_create(outGraph,nodeAttr,numRuns);
     
     #-Main Operating Block-
     
     ##EXAMPLE: removing node 'C' then adding node 'D' and re-routing to there
-    #try: pathMatrix = nop.network_remove_node('C',outGraph,nodeAttr,nodePos,pathMatrix)    
+    #try: pathMatrix = nop.network_remove_node('C',outGraph,nodeAttr,edgeAttr,nodePos,pathMatrix)    
     #except: print("ERROR: Failed to remove node (input error)")    
-    #try: pathMatrix = nop.network_add_node('D',[1,2],['B','E'],outGraph,nodeAttr,nodePos,pathMatrix)
+    #try: pathMatrix = nop.network_add_node('D',[1,2],['B','E'],outGraph,nodeAttr,edgeAttr,nodePos,pathMatrix)
     #except: print("ERROR: Failed to add node (input error)")    
     #pathDesired = ('B','E')
     
@@ -72,57 +77,64 @@ def main():
     #-Graph Drawing-
     try:
         #Output the best path
-        print("Best path to destination from {0} to {1} is to take {2}".format(pathDesired[0],pathDesired[1],pathMatrix[pathDesired][1]))
+        print("Best path to destination from {0} to {1} is to take {2}".format(pathDesired[0],pathDesired[1],pathMatrix[pathDesired][0][1]))
         #Set the image size in inches
         rcParams['figure.figsize'] = 10, 10
         #Define a list of colors that each edge will have based on route desired
-        edge_colors = ['gray' if not i in pathMatrix[pathDesired][1] else 'green' for i in outGraph.edges()]
+        edge_colors = ['gray' if not i in pathMatrix[pathDesired][0][1] else 'green' for i in outGraph.edges()]
         #Set sizes of edges and nodes; edges scale based on whether or not in path
-        edge_width = [4 if not i in pathMatrix[pathDesired][1] else 5 for i in outGraph.edges()]
-        node_sizes = [900 for k in outGraph.nodes()]
+        edge_width = [4 if not i in pathMatrix[pathDesired][0][1] else 5 for i in outGraph.edges()]
+        node_sizes = [500 for k in outGraph.nodes()]
         #Color nodes based on whether or not in path
         node_colors = [(0.7,0.7,0.7) for k in outGraph.nodes()]
         l = 0
         for k in outGraph.nodes():    
-            for i,j in pathMatrix[pathDesired][1]:
+            for i,j in pathMatrix[pathDesired][0][1]:
                 if i==k or j==k: node_colors[l] = (0.3,1,0.4)
             l+=1
         #Calculate the average 'weight' value for edges
         edge_avg_weight = 0
         for i in outGraph.edges(data=True):
-            edge_avg_weight+=i[2]['weight']
+            edge_avg_weight+=i[2]['distance']
         edge_avg_weight/=len(outGraph.edges())
         #Set labels for edges with position based on the average weight
         for k in outGraph.nodes():
-            p = False
-            for i,j in pathMatrix[pathDesired][1]:
+            p = False #Set p based on whether or not in best path
+            for i,j in pathMatrix[pathDesired][0][1]:
                 if i==k or j==k: p = True
-            if p==True: plt.text(nodePos[k][0],nodePos[k][1]-(edge_avg_weight/6),s=k,fontsize=14,bbox=dict(facecolor='green', alpha=0.5),horizontalalignment='center')
-            else: plt.text(nodePos[k][0],nodePos[k][1]-(edge_avg_weight/6),s=k,fontsize=14,bbox=dict(facecolor='gray',alpha=0.5),horizontalalignment='center')
-        edge_labels=dict([((u,v,),str(math.floor(d['weight']*100))) for u,v,d in outGraph.edges(data=True)])
+            #If in best path, highlight text
+            if p==True: plt.text(nodePos[k][0],nodePos[k][1]+(edge_avg_weight*10),s=k,fontsize=14,bbox=dict(facecolor='green', alpha=0.5),horizontalalignment='center')
+            else: plt.text(nodePos[k][0],nodePos[k][1]+(edge_avg_weight*10),s=k,fontsize=14,bbox=dict(facecolor='gray',alpha=0.5),horizontalalignment='center')
+        edge_labels=dict([((u,v,),str(math.floor(d['weight']))) for u,v,d in outGraph.edges(data=True)])
     except:
         print("WARNING: Graph cannot display best path due to an error.")
         nx.draw(outGraph,nodePos)
     else:
+        #Load Map Image
+        img = imread('files/schematic_nolabels.png')
+        plt.imshow(img, zorder=0, extent=[0, 100, 100, 0], aspect='auto')        
         #Set the graph title
         plt.title('Best Route from {0} to {1}'.format(pathDesired[0],pathDesired[1]),fontsize=18)
         #Draw the graph       
         nx.draw(outGraph,nodePos,node_color=node_colors,node_size=node_sizes,node_shape='s',linewidths=1.5,edge_color=edge_colors,width=edge_width)
         #Draw the edge labels
         nx.draw_networkx_edge_labels(outGraph,nodePos,edge_labels=edge_labels,label_pos=0.5) 
+        #Draw information box
+        plt.text(1,15,'Travel Time: {0} s\nTotal Distance: {1} mi\n# of Runs Tested: {2}\n'.format(round(pathMatrix[pathDesired][0][2],3),round(pathMatrix[pathDesired][0][3],3),numRuns),fontsize=12,bbox=dict(facecolor='green', alpha=0.5),horizontalalignment='left')        
         #Save the graph to an image file        
         plt.savefig('files/output.png')
         #Display the graph
         plt.show()
-
+        #for i in range(0,numRuns): print('Run {0} - Best Path Time: {1}; Distance: {2}'.format(i,pathMatrix[pathDesired][1][i][2],pathMatrix[pathDesired][1][i][3]))
+            
 #--Execute Program--
+nodeNeighbors = list();
 #Check to see if we are using a file
 if loadFromFile == True:
     fileType = nop.file_check(fileDir)
     #If the output isn't a bool, it's an excel workbook
-    if type(fileType)!=bool: pathDesired = nop.file_excel_interpret(fileDir,fileType,nodeAttr,nodeNeighbors,pathDesired)
-
+    if type(fileType)!=bool: pathDesired = nop.file_excel_interpret(fileDir,fileType,nodeAttr,edgeAttr,nodeNeighbors,pathDesired)
 #Before doing anything else, check user input for errors
-netProceed = nop.check_user_input(nodeAttr,nodeNeighbors,pathDesired)
+netProceed = nop.check_user_input(nodeAttr,edgeAttr,nodeNeighbors,pathDesired,numRuns)
 if netProceed == False: print("Please reconfigure the model to fix the issue(s) and try again.")
 else: main()
